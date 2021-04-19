52aa509bfb437e43c74d32cff28d9cdb8764416f
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-04-19 00:08:09 -0300

    update lesson preview

--- src/pages/manager/lessons/[id]/preview.tsx
+++ src/pages/manager/lessons/[id]/preview.tsx
@@ -40,7 +40,7 @@ const PreviewLessonPage = () => {
                     <div className="title-wrap">
                         <h2 className="title-5 text-center">
                             {/* <i className="fa fa-edit mr-2"></i> Editar conteúdo */}
-                            <span className="badge badge-light mr-2"><small>PRÉVIA</small></span>{resource.name ?? 'Sem título'} - {resource.discipline ?? ''}
+                            <span className="badge badge-light mr-2"><small>PRÉVIA</small></span>{resource.name ?? 'Sem título'}
                         </h2>
                         <Button color="light" onClick={() => router.back()}><i className="fa fa-arrow-left mr-2"></i>Voltar</Button>
 
@@ -51,6 +51,11 @@ const PreviewLessonPage = () => {
                 <div className="col-12">
                     <div className="card m-b-70">
                         <div className="card-body">
+                            <div className="row mb-3">
+                                <div className="col">
+                                    <h1>{resource.name}</h1>
+                                </div>
+                            </div>
                             <div className="row">
                                 <div className="col-md-12">
                                     {parseHtml(resource?.content ?? '')}
@@ -59,7 +64,53 @@ const PreviewLessonPage = () => {
                         </div>
                     </div>
                 </div>
-            </div> </> : <LoadingComponent />}
+            </div>
+
+            {/* Informações Adicionais */}
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4>Informações</h4>
+                        </div>
+                        <div className="card-body">
+                            <div className="submenu-jogo">
+                                <nav>
+                                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                                        <a className="nav-item nav-link active" id="submenu-jogo-materialdeapoio-tab" data-toggle="tab" href="#submenu-jogo-materialdeapoio"
+                                            role="tab" aria-controls="custom-nav-profile" aria-selected="false">Material de Apoio</a>
+                                        <a className="nav-item nav-link" id="submenu-jogo-instrucoes-tab" data-toggle="tab" href="#submenu-jogo-instrucoes" role="tab"
+                                            aria-controls="custom-nav-home" aria-selected="true">Instruções</a>
+                                        <a className="nav-item nav-link" id="submenu-jogo-instrucoeslibras-tab" data-toggle="tab" href="#submenu-jogo-instrucoeslibras"
+                                            role="tab" aria-controls="custom-nav-profile" aria-selected="false">Instruções em Libras</a>
+                                        <a className="nav-item nav-link" id="#submenu-jogo-historico-tab" data-toggle="tab" href="#submenu-jogo-historico" role="tab"
+                                            aria-controls="custom-nav-contact" aria-selected="false">Histórico de Pontuações</a>
+                                    </div>
+                                </nav>
+                                <div className="tab-content pl-3 pt-2" id="nav-tabContent">
+                                    <div className="tab-pane fade " id="submenu-jogo-instrucoes" role="tabpanel" aria-labelledby="submenu-jogo-instrucoes-tab">
+                                        <p>Instruções</p>
+                                    </div>
+                                    <div className="tab-pane fade" id="submenu-jogo-instrucoeslibras" role="tabpanel" aria-labelledby="submenu-jogo-instrucoeslibras-tab">
+                                        <p>Video de libras</p>
+                                    </div>
+                                    <div className="tab-pane fade show active materialdeapoio" id="submenu-jogo-materialdeapoio" role="tabpanel" aria-labelledby="submenu-jogo-materialdeapoio-tab">
+                                        <h4>Algumas Informações</h4>
+
+                                    </div>
+                                    <div className="tab-pane fade" id="submenu-jogo-historico" role="tabpanel" aria-labelledby="#submenu-jogo-historico-tab">
+                                        <p>Histórico pontuacoes</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </> : <LoadingComponent />
+        }
     </AppLeftNavigation >
 }
 
---
+++ src/utils/hashToColor.ts
@@ -0,0 +1,14 @@
+const hashToColor = (input: string) => {
+    let hash = 0;
+    for (let i = 0; i < input.length; i++) {
+        hash = input.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let color = '#';
+    for (let i = 0; i < 3; i++) {
+        let value = (hash >> (i * 8)) & 0xFF;
+        color += ('00' + value.toString(16)).substr(-2);
+    }
+    return color;
+}
+
+export { hashToColor }
