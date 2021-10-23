42a48ce684521b90a85c60cd9726254b65c9a94c
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-23 02:59:02 -0300

    update layout manager -glossary

--- src/pages/manager/glossary/[id]/update.tsx
+++ src/pages/manager/glossary/[id]/update.tsx
@@ -1,23 +1,19 @@
-import { Form } from "@unform/web"
+import { Button, Card, Form, Input, PageHeader, Select } from "antd"
+import { useForm } from "antd/lib/form/Form"
 import { useRouter } from "next/router"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
-import { LoadingWrapper } from "../../../../components/Loading/Loading"
-import { Button } from "../../../../components/UI/Button"
-import { Input } from "../../../../components/UI/Input"
-import RichTextEditor from "../../../../components/UI/RichTextEditor"
-import Select from "../../../../components/UI/Select"
-import { AppLeftNavigation } from "../../../../layouts/AppLeftNavigation"
-import { ValidateForm, Yup } from "../../../../plugins/validation/FormValidator"
+import { RichTextSunEditor } from "../../../../components/UI/RichTextSunEditor"
+import { AdminLayout } from "../../../../layouts/AdminLayout"
 import { DisciplineService } from "../../../../services/DisciplineService"
 import { GlossaryService } from "../../../../services/GlossaryService"
 import { SubjectService } from "../../../../services/SubjectService"
 
 
 const UpdateGlossaryPage = () => {
-    const [resource, setResource] = useState(null)
-    const formRef = useRef(null)
+    const [form] = useForm()
     const [isLoading, setIsLoading] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
     const [disciplines, setDisciplines] = useState(null)
     const [subjects, setSubjects] = useState(null)
     const router = useRouter()
@@ -37,113 +33,87 @@ const UpdateGlossaryPage = () => {
 
     const loadSubjects = async (discipline_id: string) => {
         if (!discipline_id) return
-        triggerClearSelect()
-        const subjects = await SubjectService.getAllFromDiscipline(discipline_id)
+        const subjects = await SubjectService.getFromDisciplines(discipline_id)
         setSubjects(subjects)
     }
-    const triggerClearSelect = () => null
 
+    const handleValuesChange = (changes) => {
+        if (changes.discipline) {
+            loadSubjects(changes.discipline)
+            form.resetFields(['subject'])
+        }
+    }
 
     const loadResource = async () => {
+        setIsLoading(true)
         const id = router.query.id as string
         if (!id) return
         const resource = await GlossaryService.get(id)
-        // setTimeout(() => {
-        setResource(resource)
-        if (!resource || !resource.id) {
+
+        if (!resource || !resource._id) {
             toast('Conteúdo não encontrado!', { type: 'error' })
             return setTimeout(() => router.push('/manager/glossary'), 4000)
         }
-
-        formRef.current.setData(resource)
-
-        // }, 1000)
-
-
-
+        await loadSubjects(resource.discipline)
+        form.setFieldsValue(resource)
+        setIsLoading(false)
     }
 
     const handleSubmit = async (data) => {
-        const isValid = await ValidateForm({
-            name: Yup.string().required().min(1),
-            description: Yup.string().required().min(3),
-            discipline: Yup.string().required().uuid('este campo necessita ser preenchido'),
-            // subject: Yup.string().required().uuid('este campo necessita ser preenchido'),
-        }, data, formRef)
-
-        if (isValid) {
-            try {
-                setIsLoading(true)
-                await GlossaryService.update(router.query.id as string, data)
-                setIsLoading(false)
-                toast("Conteúdo atualizado com sucesso!", { type: 'success' })
-            } catch (error) {
-                setIsLoading(false)
-                toast(error.response.data.message, { type: 'error' })
-            }
+        try {
+            setIsSubmitting(true)
+            await GlossaryService.update(router.query.id as string, data)
+            toast("Conteúdo atualizado com sucesso!", { type: 'success' })
+        } catch (error) {
+            toast(error.response.data.message, { type: 'error' })
+        } finally {
+            setIsSubmitting(false)
         }
     }
 
-    return <AppLeftNavigation>
-        <LoadingWrapper isLoading={!resource}>
-            <div className="row m-b-20">
-                <div className="col-md-12">
-                    <div className="title-wrap">
-                        <h2 className="title-5 text-center">
-                            <i className="fa fa-edit mr-2"></i> Editar conteúdo
-                    </h2>
-                        <Button color="light" onClick={() => router.back()}><i className="fa fa-arrow-left mr-2"></i>Voltar</Button>
-
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <div className="card m-b-70">
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <Form ref={formRef} onSubmit={handleSubmit} className="row">
-                                        <div className="col-12">
-                                            <h4>Informações básicas</h4>
-                                            <hr />
-                                        </div>
-                                        <div className="col-md-12">
-                                            <Input label="Termo:" name="name" />
-                                        </div>
-                                        <div className="col-md-12">
-                                            <RichTextEditor name="description" label="Descrição:" onSave={() => formRef.current.submitForm()} />
-                                        </div>
-                                        <div className="col-md-12">
-                                            {disciplines && <Select
-                                                label="Disciplina:"
-                                                name="discipline"
-                                                onChange={e => loadSubjects(e?.value)}
-                                                options={disciplines.map(e => ({ label: e.name, value: e.id }))}
-                                            />}
-                                        </div>
-                                        <div className="col-md-12">
-                                            {subjects && <Select
-                                                label="Assunto:"
-                                                name="subject"
-                                                clear={triggerClearSelect}
-                                                options={subjects?.map(e => ({ label: e.name, value: e.id }))}
-                                            />}
-                                        </div>
-                                        <div className="col-md-12">
-                                            <Button color="success" block isLoading={isLoading}>
-                                                Salvar
-                                        </Button>
-                                        </div>
-                                    </Form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </LoadingWrapper>
-    </AppLeftNavigation >
+    return <AdminLayout>
+        <PageHeader
+            title="Editar item do glossário"
+            onBack={() => router.push('/manager/glossary')}
+        // breadcrumb={{ routes }}
+        // subTitle="This is a subtitle"
+        />
+        <Card title="Informações básicas" loading={isLoading}>
+            <Form name="basic" form={form} layout="vertical" onValuesChange={handleValuesChange} onFinish={handleSubmit} onFinishFailed={null}>
+                <Form.Item label="Nome do termo" name="name" rules={[{ required: true, min: 1 }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Descrição" name="description" rules={[{ required: true }]}>
+                    <RichTextSunEditor />
+                </Form.Item>
+                <Form.Item label="Disciplina" name="discipline" rules={[{ required: true }]}>
+                    <Select
+                        showSearch
+                        placeholder="Selecione uma disciplina"
+                        optionFilterProp="children"
+                        filterOption={(input, option) =>
+                            option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                    >
+                        {disciplines?.map(v => <Select.Option value={v._id}>{v.name}</Select.Option>)}
+                    </Select>
+                </Form.Item>
+                <Form.Item label="Assunto" name="subject" rules={[{ required: true }]}>
+                    <Select
+                        showSearch
+                        placeholder="Selecione um assunto"
+                        optionFilterProp="children"
+                        filterOption={(input, option) =>
+                            option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                    >
+                        {subjects?.map(v => <Select.Option value={v._id}>{v.name}</Select.Option>)}
+                    </Select>
+                </Form.Item>
+                <Button type="primary" htmlType="submit" loading={isSubmitting}>Salvar</Button>
+            </Form>
+        </Card>
+    </AdminLayout >
 }
 
 export default UpdateGlossaryPage
--- src/pages/manager/glossary/create.tsx
+++ src/pages/manager/glossary/create.tsx
@@ -1,20 +1,17 @@
-import { Form } from "@unform/web"
+import { Button, Card, Form, Input, PageHeader, Select } from "antd"
+import { useForm } from "antd/lib/form/Form"
 import { useRouter } from "next/router"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
-import { Button } from "../../../components/UI/Button"
-import { Input } from "../../../components/UI/Input"
-import RichTextEditor from "../../../components/UI/RichTextEditor"
-import Select from "../../../components/UI/Select"
-import { AppLeftNavigation } from "../../../layouts/AppLeftNavigation"
-import { ValidateForm, Yup } from "../../../plugins/validation/FormValidator"
+import { RichTextSunEditor } from "../../../components/UI/RichTextSunEditor"
+import { AdminLayout } from "../../../layouts/AdminLayout"
 import { DisciplineService } from "../../../services/DisciplineService"
 import { GlossaryService } from "../../../services/GlossaryService"
 import { SubjectService } from "../../../services/SubjectService"
 
 const CreateGlossaryItemPage = () => {
-    const formRef = useRef(null)
-    const [isLoading, setIsLoading] = useState(false)
+    const [form] = useForm()
+    const [isSubmitting, setIsSubmitting] = useState(false)
     const [disciplines, setDisciplines] = useState(null)
     const [subjects, setSubjects] = useState(null)
     const router = useRouter()
@@ -30,95 +27,75 @@ const CreateGlossaryItemPage = () => {
 
     const loadSubjects = async (discipline_id: string) => {
         if (!discipline_id) return
-        triggerClearSelect()
-        const subjects = await SubjectService.getAllFromDiscipline(discipline_id)
+        const subjects = await SubjectService.getFromDisciplines(discipline_id)
         setSubjects(subjects)
     }
-    const triggerClearSelect = () => null
 
-    const handleSubmit = async (data) => {
-        const isValid = await ValidateForm({
-            name: Yup.string().required().min(1),
-            description: Yup.string().required().min(3),
-            discipline: Yup.string().required().uuid('este campo necessita ser preenchido'),
-            // subject: Yup.string().required().uuid('este campo necessita ser preenchido'),
-        }, data, formRef)
-
-        if (isValid) {
-            try {
-                setIsLoading(true)
-                await GlossaryService.create(data)
-                setIsLoading(false)
-                toast("Item criado com sucesso!", { type: 'success' })
-                setTimeout(() => {
-                    router.push('/manager/glossary')
-                }, 2000);
-            } catch (error) {
-                setIsLoading(false)
-                alert(error.response.data.message)
-            }
+    const handleValuesChange = (changes) => {
+        if (changes.discipline) {
+            loadSubjects(changes.discipline)
+            form.resetFields(['subject'])
         }
     }
 
-    return <AppLeftNavigation>
-        <div className="row m-b-20">
-            <div className="col-md-12">
-                <div className="title-wrap">
-                    <h2 className="title-5 text-center">
-                        <i className="fa fa-plus mr-2"></i> Novo item do glossário
-                    </h2>
-                    <Button color="light" onClick={() => router.back()}><i className="fa fa-arrow-left mr-2"></i>Voltar</Button>
+    const handleSubmit = async (data) => {
+        try {
+            setIsSubmitting(true)
+            await GlossaryService.create(data)
+            toast("Item criado com sucesso!", { type: 'success' })
+            setTimeout(() => {
+                router.push('/manager/glossary')
+            }, 2000);
+        } catch (error) {
+            alert(error.response.data.message)
+        } finally {
+            setIsSubmitting(false)
+        }
+    }
 
-                </div>
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-12">
-                <div className="card m-b-70">
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <Form ref={formRef} onSubmit={handleSubmit} className="row">
-                                    <div className="col-12">
-                                        <h4>Informações básicas</h4>
-                                        <hr />
-                                    </div>
-                                    <div className="col-md-12">
-                                        <Input label="Termo:" name="name" />
-                                    </div>
-                                    <div className="col-md-12">
-                                        <RichTextEditor name="description" label="Descrição:" onSave={() => formRef.current.submitForm()} />
-                                    </div>
-                                    <div className="col-md-12">
-                                        {disciplines && <Select
-                                            label="Disciplina:"
-                                            name="discipline"
-                                            onChange={e => loadSubjects(e?.value)}
-                                            options={disciplines.map(e => ({ label: e.name, value: e.id }))}
-                                        />}
-                                    </div>
-                                    <div className="col-md-12">
-                                        {subjects && <Select
-                                            label="Assunto:"
-                                            name="subject"
-                                            clear={triggerClearSelect}
-                                            options={subjects?.map(e => ({ label: e.name, value: e.id }))}
-                                        />}
-                                    </div>
-
-                                    <div className="col-md-12">
-                                        <Button color="success" block isLoading={isLoading}>
-                                            Salvar
-                                        </Button>
-                                    </div>
-                                </Form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </AppLeftNavigation >
+    return <AdminLayout>
+        <PageHeader
+            title="Criar novo item do glossário"
+            onBack={() => router.push('/manager/glossary')}
+        // breadcrumb={{ routes }}
+        // subTitle="This is a subtitle"
+        />
+        <Card title="Informações básicas">
+            <Form name="basic" form={form} layout="vertical" onValuesChange={handleValuesChange} onFinish={handleSubmit} onFinishFailed={null}>
+                <Form.Item label="Nome do termo" name="name" rules={[{ required: true, min: 1 }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Descrição" name="description" rules={[{ required: true }]}>
+                    <RichTextSunEditor />
+                </Form.Item>
+                <Form.Item label="Disciplina" name="discipline" rules={[{ required: true }]}>
+                    <Select
+                        showSearch
+                        placeholder="Selecione uma disciplina"
+                        optionFilterProp="children"
+                        filterOption={(input, option) =>
+                            option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                    >
+                        {disciplines?.map(v => <Select.Option value={v._id}>{v.name}</Select.Option>)}
+                    </Select>
+                </Form.Item>
+                <Form.Item label="Assunto" name="subject" rules={[{ required: true }]}>
+                    <Select
+                        showSearch
+                        placeholder="Selecione um assunto"
+                        optionFilterProp="children"
+                        filterOption={(input, option) =>
+                            option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                    >
+                        {subjects?.map(v => <Select.Option value={v._id}>{v.name}</Select.Option>)}
+                    </Select>
+                </Form.Item>
+                <Button type="primary" htmlType="submit" loading={isSubmitting}>Salvar</Button>
+            </Form>
+        </Card>
+    </AdminLayout >
 }
 
 export default CreateGlossaryItemPage
