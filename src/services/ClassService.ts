9f8af4aace6cf3f48b23eebe9e7b8040b979d7a9
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-23 00:34:34 -0300

    update class service

--- src/services/ClassService.ts
+++ src/services/ClassService.ts
@@ -1,7 +1,11 @@
 import Axios from "../plugins/http/axios.instance"
 
+interface ICreateReq {
+    name: string
+    discipline: string
+}
 class ClassService {
-    static async create(data) {
+    static async create(data: ICreateReq) {
         return (await Axios.post(`/classes`, data)).data.data
     }
 
@@ -9,16 +13,32 @@ class ClassService {
         return (await Axios.get(`/classes/${id}`)).data.data
     }
 
+    static async getPeople(id: string) {
+        return (await Axios.get(`/classes/${id}/people`)).data.data
+    }
+
     static async list(page?: number) {
         return (await Axios.get(`/classes`, { params: { page } })).data
     }
 
-    static async update(id: string, data) {
+    static async update(id: string, data: ICreateReq) {
         return (await Axios.post(`/classes/${id}`, data)).data.data
     }
 
     static async delete(id: string) {
         return (await Axios.delete(`/classes/${id}`)).data.data
+    }
+
+    static async getMyClasses() {
+        return (await Axios.get(`/classes/my-classes`)).data.data
+    }
+
+    static async enroll(code: string) {
+        return (await Axios.post(`/classes/enroll`, { code })).data.data
+    }
+    
+    static async unenroll(classId: string) {
+        return (await Axios.post(`/classes/unenroll`, { classId })).data.data
     }
 }
 
