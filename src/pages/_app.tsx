85a277ff8231630cc0363e9bcbff36d8b5450167
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-23 02:41:39 -0300

    add antd app provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
-import AppContext, { AppProvider, useApp } from '../context/AppContext'
-import '../plugins/validation/yup-translation'
-import 'react-toastify/dist/ReactToastify.css';
+import { ConfigProvider } from 'antd';
+import Head from 'next/head';
+import ptBR from 'antd/lib/locale/pt_BR';
 import { ToastContainer } from 'react-toastify';
+import 'suneditor/dist/css/suneditor.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import "../../styles/antd.css";
+import { Loading } from '../components/Loading/Loading2';
+import { AppProvider } from '../context/AppContext';
+import '../plugins/validation/yup-translation';
 
 function LabvisApp({ Component, pageProps }) {
-  return <AppProvider>
-    <Component {...pageProps} />
-    <ToastContainer />
-  </AppProvider >
+  return <ConfigProvider locale={ptBR}>
+    <AppProvider>
+      <Head>
+        <title>Laboratório Virtual</title>
+        </Head>
+      <Loading />
+      <Component {...pageProps} />
+      <ToastContainer />
+    </AppProvider >
+  </ConfigProvider>
 }
 
 export default LabvisApp
