022c031e355e5d12c32c63f1efbb3870246063e3
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-23 02:51:37 -0300

    update layout manager -subjects

--- src/pages/manager/subjects/[id]/update.tsx
+++ src/pages/manager/subjects/[id]/update.tsx
@@ -1,20 +1,16 @@
-import { Form } from "@unform/web"
+import { Button, Card, Form, Input, PageHeader, Select } from "antd"
+import { useForm } from "antd/lib/form/Form"
 import { useRouter } from "next/router"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
-import { LoadingWrapper } from "../../../../components/Loading/Loading"
-import { Button } from "../../../../components/UI/Button"
-import { Input } from "../../../../components/UI/Input"
-import Select from "../../../../components/UI/Select"
-import { AppLeftNavigation } from "../../../../layouts/AppLeftNavigation"
-import { ValidateForm, Yup } from "../../../../plugins/validation/FormValidator"
+import { AdminLayout } from "../../../../layouts/AdminLayout"
 import { DisciplineService } from "../../../../services/DisciplineService"
 import { SubjectService } from "../../../../services/SubjectService"
 
 
 const UpdateSubjectPage = () => {
-    const [resource, setResource] = useState(null)
-    const formRef = useRef(null)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [form] = useForm()
     const [isLoading, setIsLoading] = useState(false)
     const router = useRouter()
 
@@ -31,84 +27,61 @@ const UpdateSubjectPage = () => {
     }
 
     const loadResource = async () => {
+        setIsLoading(true)
         const id = router.query.id as string
         if (!id) return
         const resource = await SubjectService.get(id)
-        setResource(resource)
 
-        if (!resource || !resource.id) {
+        if (!resource || !resource._id) {
             toast('Assunto não encontrada!', { type: 'error' })
             return setTimeout(() => router.push('/manager/classes'), 4000)
         }
 
-        formRef.current.setData(resource)
+        form.setFieldsValue(resource)
+        setIsLoading(false)
     }
 
     const handleSubmit = async (data) => {
-        const isValid = await ValidateForm({
-            name: Yup.string().required().min(3),
-            discipline: Yup.string().required().uuid('este campo deve ser preenchido')
-        }, data, formRef)
-
-        if (isValid) {
             try {
-                setIsLoading(true)
+                setIsSubmitting(true)
                 const id = router.query.id as string
                 await SubjectService.update(id, data)
-                setIsLoading(false)
                 toast("Assunto atualizada com sucesso!", { type: 'success' })
             } catch (error) {
-                setIsLoading(false)
                 toast(error.response.data.message, { type: 'error' })
+            } finally {
+                setIsSubmitting(false)
             }
-        }
     }
 
-    return <AppLeftNavigation>
-        <LoadingWrapper isLoading={!resource}>
-            <div className="row m-b-20">
-                <div className="col-md-12">
-                    <div className="title-wrap">
-                        <h2 className="title-5 text-center">
-                            <i className="fa fa-edit mr-2"></i> Editar assunto
-                    </h2>
-                        <Button color="light" onClick={() => router.back()}><i className="fa fa-arrow-left mr-2"></i>Voltar</Button>
-
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <div className="card m-b-70">
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <Form ref={formRef} onSubmit={handleSubmit} className="row">
-                                        <div className="col-12">
-                                            <h4>Informações básicas</h4>
-                                            <hr />
-                                        </div>
-                                        <div className="col-md-12">
-                                            <Input label="Nome do assunto:" name="name" />
-                                        </div>
-                                        <div className="col-md-12">
-                                            <Select label="Disciplina:" name="discipline" options={disciplines.map(e => ({ label: e.name, value: e.id }))} />
-                                        </div>
-
-                                        <div className="col-md-12">
-                                            <Button color="success" block isLoading={isLoading}>
-                                                Salvar
-                                            </Button>
-                                        </div>
-                                    </Form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </LoadingWrapper>
-    </AppLeftNavigation >
+    return <AdminLayout>
+        <PageHeader
+            title="Editar assunto"
+            onBack={() => router.push('/manager/subjects')}
+        // breadcrumb={{ routes }}
+        // subTitle="This is a subtitle"
+        />
+        <Card title="Informações básicas">
+            <Form name="basic" form={form} layout="vertical" onFinish={handleSubmit} onFinishFailed={null}>
+                <Form.Item label="Nome do assunto" name="name" rules={[{ required: true, min: 3 }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Disciplina" name="discipline" rules={[{ required: true }]}>
+                    <Select
+                        showSearch
+                        placeholder="Selecione uma disciplina"
+                        optionFilterProp="children"
+                        filterOption={(input, option) =>
+                            option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                    >
+                        {disciplines.map(v => <Select.Option value={v._id}>{v.name}</Select.Option>)}
+                    </Select>
+                </Form.Item>
+                <Button type="primary" htmlType="submit" loading={isSubmitting}>Salvar</Button>
+            </Form>
+        </Card>
+    </AdminLayout >
 }
 
 export default UpdateSubjectPage
--- src/pages/manager/subjects/create.tsx
+++ src/pages/manager/subjects/create.tsx
@@ -1,20 +1,15 @@
-import { Form } from "@unform/web"
+import { Button, Card, Form, Input, PageHeader, Select } from "antd"
+import { useForm } from "antd/lib/form/Form"
 import { useRouter } from "next/router"
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { toast } from "react-toastify"
-import { AsyncSelect } from "../../../components/UI/AsyncSelect"
-import { Button } from "../../../components/UI/Button"
-import { Input } from "../../../components/UI/Input"
-import Select from "../../../components/UI/Select"
-// import AsyncSelect from "../../../components/UI/Select"
-import { AppLeftNavigation } from "../../../layouts/AppLeftNavigation"
-import { ValidateForm, Yup } from "../../../plugins/validation/FormValidator"
+import { AdminLayout } from "../../../layouts/AdminLayout"
 import { DisciplineService } from "../../../services/DisciplineService"
 import { SubjectService } from "../../../services/SubjectService"
 
 const CreateSubjectPage = () => {
-    const formRef = useRef(null)
-    const [isLoading, setIsLoading] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [form] = useForm()
     const [disciplines, setDisciplines] = useState([])
     const router = useRouter()
 
@@ -22,75 +17,51 @@ const CreateSubjectPage = () => {
         loadDisciplines()
     }, [])
 
-    const loadDisciplines = async (query?: string) => {
-        console.log(query)
+    const loadDisciplines = async () => {
         const disciplines = await DisciplineService.list()
         setDisciplines(disciplines.data)
     }
 
     const handleSubmit = async (data) => {
-        const isValid = await ValidateForm({
-            name: Yup.string().required().min(3),
-            discipline: Yup.string().required().uuid('este campo necessita ser preenchido')
-        }, data, formRef)
-
-        if (isValid) {
             try {
-                setIsLoading(true)
+                setIsSubmitting(true)
                 await SubjectService.create(data)
-                setIsLoading(false)
                 toast("Assunto criado com sucesso!", { type: 'success' })
             } catch (error) {
-                setIsLoading(false)
                 alert(error.response.data.message)
+            } finally {
+                setIsSubmitting(false)
             }
-        }
     }
 
-    return <AppLeftNavigation>
-        <div className="row m-b-20">
-            <div className="col-md-12">
-                <div className="title-wrap">
-                    <h2 className="title-5 text-center">
-                        <i className="fa fa-plus mr-2"></i> Nova assunto
-                    </h2>
-                    <Button color="light" onClick={() => router.back()}><i className="fa fa-arrow-left mr-2"></i>Voltar</Button>
-
-                </div>
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-12">
-                <div className="card m-b-70">
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <Form ref={formRef} onSubmit={handleSubmit} className="row">
-                                    <div className="col-12">
-                                        <h4>Informações básicas</h4>
-                                        <hr />
-                                    </div>
-                                    <div className="col-md-12">
-                                        <Input label="Nome do assunto:" name="name" />
-                                    </div>
-                                    <div className="col-md-12">
-                                        <Select label="Disciplina:" name="discipline" options={disciplines.map(e => ({ label: e.name, value: e.id }))} />
-                                        {/* <AsyncSelect label="Disciplina:" name="discipline" executeSearch={searchDiscipline}/> */}
-                                    </div>
-
-                                    <div className="col-md-12">
-                                        <Button color="success" block isLoading={isLoading}>
-                                            Salvar
-                                            </Button>
-                                    </div>
-                                </Form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </AppLeftNavigation >
+    return <AdminLayout>
+        <PageHeader
+            title="Criar novo assunto"
+            onBack={() => router.push('/manager/subjects')}
+        // breadcrumb={{ routes }}
+        // subTitle="This is a subtitle"
+        />
+        <Card title="Informações básicas">
+            <Form name="basic" form={form} layout="vertical" onFinish={handleSubmit} onFinishFailed={null}>
+                <Form.Item label="Nome do assunto" name="name" rules={[{ required: true, min: 3 }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Disciplina" name="discipline" rules={[{ required: true }]}>
+                    <Select
+                        showSearch
+                        placeholder="Selecione uma disciplina"
+                        optionFilterProp="children"
+                        filterOption={(input, option) =>
+                            option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                    >
+                        {disciplines.map(v => <Select.Option value={v._id}>{v.name}</Select.Option>)}
+                    </Select>
+                </Form.Item>
+                <Button type="primary" htmlType="submit" loading={isSubmitting}>Salvar</Button>
+            </Form>
+        </Card>
+    </AdminLayout>
 }
 
 export default CreateSubjectPage
