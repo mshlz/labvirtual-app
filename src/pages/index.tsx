66236bfac53f902b4256dd9a9cd8ac05eb3127b1
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-23 02:45:11 -0300

    update index layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import { toast } from "react-toastify"
 import { Button } from "../components/UI/Button"
 import { useApp } from "../context/AppContext"
-import { AppLeftNavigation } from "../layouts/AppLeftNavigation"
+import { AdminLayout } from "../layouts/AdminLayout"
 
 const Index = () => {
     const { user } = useApp()
-    return <AppLeftNavigation>
+    return <AdminLayout>
         <h1>Bem vindo, {user.name}</h1>
         <Button color="success" onClick={() => toast("Test toast")}>Test toast</Button>
-    </AppLeftNavigation>
+    </AdminLayout>
 }
 
 export default Index
