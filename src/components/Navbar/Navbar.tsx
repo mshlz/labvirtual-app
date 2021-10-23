db57347882d1f2399d8390efd669f31be27ccbef
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-23 01:40:58 -0300

    migrate navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,51 @@
-import { AccountBox } from "./components/AccountBox"
-import { NotificationBox } from "./components/NotificationBox"
-import { SearchBox } from "./components/SearchBox"
+import { MenuOutlined, LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Dropdown, Menu, Space, Typography } from "antd";
+import Avatar from "antd/lib/avatar/avatar";
+import React from "react";
+import { useApp } from "../../context/AppContext";
+import styles from "./Navbar.module.css";
 
 const Navbar = (props) => {
-
-  return (<>
-    {/* <!-- HEADER DESKTOP--> */}
-    <header className="header-desktop">
-      <div className="section__content section__content--p30">
-        <div className="container-fluid">
-          <div className="header-wrap">
-            <SearchBox />
-
-            <div className="header-button">
-              {/* <NotificationBox /> */}
-              <AccountBox />
-            </div>
-
-          </div>
-        </div>
+  const { logout } = useApp()
+  return (
+    <nav className={styles.navbar}>
+      <div>
+        <Button
+          className={styles.menu}
+          type="ghost"
+          icon={<MenuOutlined />}
+          onClick={() => props.handleSidebar(true)}
+        />
+        <a href="/"><img src={'/assets/images/logo.png'} className={styles.logo} alt="logo" /></a>
       </div>
-    </header>
-    {/* <!-- HEADER DESKTOP--> */}
-  </>)
-}
 
-export { Navbar }
+      {/* <div>
+      </div> */}
+
+      <div>
+        <Dropdown
+          overlay={
+            <Menu style={{ minWidth: 150 }}>
+              <Menu.Item key="0" icon={<UserOutlined />}>
+                {/* TODO */}
+                <a href="#">Perfil</a>
+              </Menu.Item>
+              <Menu.Divider />
+              <Menu.Item key="3" icon={<LogoutOutlined />} onClick={() => logout()}>Sair</Menu.Item>
+            </Menu>
+          }
+          arrow
+          overlayStyle={{ marginTop: '16px' }}
+          trigger={['click']}
+        >
+          <Space style={{ cursor: 'pointer' }}>
+            <Avatar icon={'MH'} />
+            <Typography.Text>Mateus</Typography.Text>
+          </Space>
+        </Dropdown>
+      </div>
+    </nav>
+  );
+};
+
+export { Navbar };
